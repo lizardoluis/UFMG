d36fb3691c3998541a37a608959d1dbdb40e9b1f
Luis Eduardo Oliveira Lizardo <lizardo@araguaia.grad.dcc.ufmg.br>
2013-08-28 15:03:09 -0300

    Classe Sequence que representa uma sequencia. Classe Align que possui o algoritmo de alinhamento.

---
+++ src/Align.h
@@ -0,0 +1,21 @@
+#include "ScoringMatrix.h"
+#include "Sequence.h"
+
+class Align {
+private:
+
+public:
+	Align();
+
+	int globalAlignment(Sequence sequenceA, Sequence sequenceB, ScoringMatrix scoringMatrix)
+	{
+		int matrix[sequenceA.size()][sequenceB.size()];
+
+		for(int i=0; i<sequenceA.size(); i++){
+			matrix[][]
+		}
+		for(int i=0; i<sequenceB.size(); i++){
+			
+		}
+	}
+}
