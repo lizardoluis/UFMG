587153592d0cae74a6006b16f2a326f1a5382084
Hugo Richard Amaral <hucharal@pomba.grad>
2013-09-11 14:56:15 -0300

    recuperacao classe Sequence

---
+++ src/main2.cpp
@@ -0,0 +1,13 @@
+//#include "ScoringMatrix.h"
+#include "Sequence.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argv, char *argc[]){
+    if (argv == 2) {
+        Sequence seq(argc[1]);
+    }
+
+	return 0;
+}
