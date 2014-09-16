661351d1e22e0b00d1357287459b963b38f983ab
lizardo <lizardo@floripa>
2014-09-16 16:46:30 -0300

    .

---
+++ src/paa-tp1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : paa-tp1.cpp
+// Author      : Luis Lizardo
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "fuck you" << endl; // prints fuck you
+	return 0;
+}
