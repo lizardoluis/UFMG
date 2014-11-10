9f0ffeed03b3e96bd42adc0bd1eb4f65e21cddd2
Luis Lizardo <lizardo.luis@gmail.com>
2014-11-10 11:50:22 -0200

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,98 @@
+//============================================================================
+// Name        : paa-tp3.cpp
+// Author      : Luis Lizardo
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#define MIN0 3
+#define MIN1 5
+
+#define CHAR0 '0'
+#define CHAR1 '1'
+#define CHAR_ '-'
+
+#define FALSE 0
+#define TRUE 1
+#define BOTH 2
+
+int processMsgBf(string msg, int index, int count, char last) {
+
+	for (unsigned i = index; i < msg.length(); i++) {
+
+		if ((last == CHAR0 && count == MIN0)
+				|| (last == CHAR1 && count == MIN1)) {
+			return TRUE;
+		}
+
+		if (msg[i] == CHAR_) {
+
+			int a, b;
+
+			if (last == CHAR0) {
+				a = processMsgBf(msg, i + 1, count + 1, CHAR0);
+				b = processMsgBf(msg, i + 1, 1, CHAR1);
+
+			} else {
+				a = processMsgBf(msg, i + 1, count + 1, CHAR1);
+				b = processMsgBf(msg, i + 1, 1, CHAR0);
+			}
+
+			if (a == TRUE && b == FALSE) return BOTH;
+			else if (a == TRUE && b == TRUE) return TRUE;
+			else return FALSE;
+
+		} else if (msg[i] == last) {
+			count++;
+		} else {
+			count = 1;
+			last = msg[i];
+		}
+	}
+
+	if ((last == CHAR0 && count == MIN0) || (last == CHAR1 && count == MIN1)) {
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+int processMsgGa(string msg){
+
+	int count=0;
+	char last="";
+
+	for(unsigned i=0; i<msg.length(); i++){
+
+		if(msg[i] == last){
+			count ++;
+		}
+		else if(msg[i] == CHAR_){
+
+		}
+		else{
+			last = msg[i];
+			count = 1;
+		}
+	}
+}
+
+int main() {
+
+	int numInstances;
+
+	cin >> numInstances;
+
+	for (int i = 0; i < numInstances; i++) {
+		string msg;
+		cin >> msg;
+
+		cout << processMsgBf(msg, 0, 0, ' ');
+	}
+
+	return 0;
+}
